f3869c9aaa7194825fd6f3870f8f55a869d564cd
Yannick Herzog <info@hit-services.net>
2016-02-26 14:24:58 +0100

    Zeige Ladeanimation für noch nicht geladene Bilder und entferne diesen, sobald Bild geladen ist. Binde Script auf allen Seiten ein.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,19 @@
 (function($){
 	console.log('ready!');
-	$('.owl-carousel').owlCarousel({
-		items: 1,
-		lazyLoad: true,
-		loop: true,
-		margin: 10
-	});
+	if ( $('.owl-carousel').length > 0 ) {
+		$('.owl-carousel').owlCarousel({
+			items: 1,
+			lazyLoad: true,
+			loop: true,
+			margin: 10
+		});
+	}
+	if ( $('.project__card-img').length > 0 ) {
+		$('.project__card-img').one('load', function (e) {
+			var that = this;
+			setTimeout(function(){
+				$(that).parent('.loading__indicator').removeClass('loading__indicator');
+			}, 300);
+		});
+	}
 })(jQuery)
